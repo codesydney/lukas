c7aee1bcb898ebf6aa05adf73f18a280583f9f6f
ettienekorayyi <corralstephenmelben@gmail.com>
2021-04-08 09:52:28 +1000

    Implemented the header.

---
+++ src/app/layout/App.js
@@ -0,0 +1,14 @@
+
+import NavBar from "../../features/nav/NavBar";
+import Container from "@material-ui/core/Container";
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment className="App" maxWidth="lg" style={{ backgroundColor: 'red'}}>
+      <NavBar />
+    </React.Fragment>
+  );
+}
+
+export default App;
