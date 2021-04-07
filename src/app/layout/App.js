d6d09b4a7ae73af446f8a1426d62936999b81a13
Stephen Melben Corral <corralstephenmelben@gmail.com>
2021-04-08 09:59:12 +1000

    Resolved Error

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,4 +1,13 @@
 
+import NavBar from "../../features/nav/NavBar";
+import Container from "@material-ui/core/Container";
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment className="App" maxWidth="lg" style={{ backgroundColor: 'red'}}>
+      <NavBar />
+    </React.Fragment>
   );
 }
 
